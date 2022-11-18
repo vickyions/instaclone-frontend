956f3cc2a446318e0751bb263db43ec378db7f09
Vicky Soni <murlimanoharsoni4@gmail.com>
2022-11-18 23:30:41 +0530

    added newPost route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Posts from "./components/PostView/Posts";
 import LandingPage from "./components/LandingPage/LandingPage";
+import NewPost from "./components/NewPost";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/posts" element={<Posts />} />
+          <Route path="/newpost" element={<NewPost />} />
           <Route path="*" element={<h1>Not FOund</h1>}/>
         </Routes>
       </BrowserRouter>
